a2ffade0f24c6260fd6c5014ce0be51e78996d93
xianpengfei <xianpengfei@bailuntec.com>
2021-03-09 11:50:56 +0800

    code

--- 205.同构字符串.js
+++ 205.同构字符串.js
@@ -1,9 +1,9 @@
 /*
  * @Description: 
  * @Author: 小羽
- * @LastEditors: 小羽
+ * @LastEditors: Please set LastEditors
  * @Date: 2021-03-05 22:50:45
- * @LastEditTime: 2021-03-09 08:47:28
+ * @LastEditTime: 2021-03-09 11:39:23
  */
 /*
  * @lc app=leetcode.cn id=205 lang=javascript
@@ -20,17 +20,26 @@
  var isIsomorphic = function(s, t) {
     let obj = {}
     for(let i = 0;i<t.length;i++){
-        if(!obj[t[i]]){
-            obj[t[i]] = s[i]
-        }else if(obj[t[i]] !== s[i]){
+        if(!obj[s[i]]){
+            let tag = true
+            for(let index in obj){
+                if(obj[index]==t[i]){
+                    tag = false
+                }
+            }
+            if(tag){
+                obj[s[i]] = t[i]
+            }else{
+                return false
+            }
+        }else if(obj[s[i]] !== t[i]){
             return false
         }
-        console.log(obj)
     }
     return true
 };
-console.log(isIsomorphic("foo","bar"))
+/* console.log(isIsomorphic("foo","bar"))
 console.log(isIsomorphic("badc","baba"))
-console.log(isIsomorphic("paper","title"))
+console.log(isIsomorphic("paper","title")) */
 // @lc code=end
 
