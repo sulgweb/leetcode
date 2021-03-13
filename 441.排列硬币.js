78d8d660545f47a8888b1c27e9771d092f220d16
xianpengfei <xianpengfei@bailuntec.com>
2021-03-13 17:18:45 +0800

    code

--- 441.排列硬币.js
+++ 441.排列硬币.js
@@ -9,8 +9,16 @@
  * @param {number} n
  * @return {number}
  */
+// 当n>tag的时候进行循环
+// 当n==tag的时候说明tag行刚好可以摆满，需要返回下一行的下标
 var arrangeCoins = function(n) {
-
+  if(n==0)return 0
+  let tag = 1
+  while(n>tag){
+    n-=tag
+    tag++
+  }
+  return n===tag?tag:tag-1
 };
 // @lc code=end
 
