9f05d426f7320f6fb4c2adc39947ffbe267c1135
冼鹏飞 <xianpengfei@cvte.com>
2021-10-20 17:38:41 +0800

    code

---
+++ 146.lru-缓存机制.js
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=146 lang=javascript
+ *
+ * [146] LRU 缓存机制
+ */
+
+// 利用es6中的map来存储键值对
+
+// @lc code=start
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+  this.capacity = capacity;
+  this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+  if (this.cache.has(key)) {
+    // 存在即更新
+    let temp = this.cache.get(key);
+    this.cache.delete(key);
+    this.cache.set(key, temp);
+    return temp;
+  }
+  return -1;
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+  if (this.cache.has(key)) {
+    // 存在即更新（删除后加入）
+    this.cache.delete(key);
+  } else if (this.cache.size >= this.capacity) {
+    // 不存在即加入
+    // 缓存超过最大值，则移除最近没有使用的
+    this.cache.delete(this.cache.keys().next().value);
+  }
+  this.cache.set(key, value);
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+// @lc code=end
+
