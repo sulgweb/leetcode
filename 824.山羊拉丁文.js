8faa79fb9391cd84e1a8b2d078572a89682fd288
冼鹏飞 <xianpengfei@cvte.com>
2021-10-21 16:28:25 +0800

    code

--- 824.山羊拉丁文.js
+++ 824.山羊拉丁文.js
@@ -9,19 +9,21 @@
 // 遍历，判断第一个字母是否为元音字母，然后进行不同的数据处理
 // 最后使用join(),将数组转回字符串
 
+// 优化，利用toLocaleLowerCase将大写转小写，可以减少数组includes的判断次数
+
 // @lc code=start
 /**
  * @param {string} sentence
  * @return {string}
  */
 var toGoatLatin = function(sentence) {
-  const arr = ['a','e','i','o','u','A','E','I','O','U'];
+  const arr = ['a','e','i','o','u'];
   const sArr = sentence.split(' ');
   let aStr = 'a'
   const res = sArr.map((item) => {
     let data;
     const firstWord = item.substr(0,1)
-    if(!arr.includes(firstWord)){
+    if(!arr.includes(firstWord.toLocaleLowerCase())){
       data = item.substr(1,item.length-1) + firstWord
     }else{
       data = item
