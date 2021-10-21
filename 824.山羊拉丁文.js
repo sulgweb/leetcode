6916969b4719343659409e3599b1dbfd74f694a3
冼鹏飞 <xianpengfei@cvte.com>
2021-10-21 16:25:03 +0800

    code

---
+++ 824.山羊拉丁文.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=824 lang=javascript
+ *
+ * [824] 山羊拉丁文
+ */
+
+// 暴力解法
+// 利用数组存储元音字母
+// 遍历，判断第一个字母是否为元音字母，然后进行不同的数据处理
+// 最后使用join(),将数组转回字符串
+
+// @lc code=start
+/**
+ * @param {string} sentence
+ * @return {string}
+ */
+var toGoatLatin = function(sentence) {
+  const arr = ['a','e','i','o','u','A','E','I','O','U'];
+  const sArr = sentence.split(' ');
+  let aStr = 'a'
+  const res = sArr.map((item) => {
+    let data;
+    const firstWord = item.substr(0,1)
+    if(!arr.includes(firstWord)){
+      data = item.substr(1,item.length-1) + firstWord
+    }else{
+      data = item
+    }
+    data += 'ma' + aStr
+    aStr += 'a'
+    return data;
+  }).join(" ")
+  return res
+};
+// @lc code=end
+
