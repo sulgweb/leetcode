aed67c1089b9828f7c1cbea1d626b7d4e9a7ef86
冼鹏飞 <xianpengfei@cvte.com>
2021-10-22 09:54:06 +0800

    code

--- 852.山脉数组的峰顶索引.js
+++ 852.山脉数组的峰顶索引.js
@@ -8,6 +8,9 @@
 // 解法1 遍历，到达山脉数组的最大值，然后返回索引
 // 解法2 先获取最大值，然后利用数组的findIndex获取索引
 
+// 优化
+// 二分法
+
 // @lc code=start
 /**
  * @param {number[]} arr
@@ -19,8 +22,17 @@ var peakIndexInMountainArray = function(arr) {
       return i
     }
   } */
-  const max = Math.max(...arr)
-  return arr.findIndex(item => item === max)
+  /* const max = Math.max(...arr)
+  return arr.findIndex(item => item === max) */
+  const len = arr.length -1
+  for(let i=0;i<=len;i++){
+    if(arr[i]>arr[i+1]){
+      return i
+    }else if(arr[len-i-1]<arr[len-i]){
+      return len-i
+    }
+  }
 };
+// console.log(peakIndexInMountainArray([24,69,100,99,79,78,67,36,26,19]))
 // @lc code=end
 
