4c2dee49ba64be21c89929f31aecf932af410fef
冼鹏飞 <xianpengfei@cvte.com>
2021-10-26 17:11:02 +0800

    code

--- 929.独特的电子邮件地址.js
+++ 929.独特的电子邮件地址.js
@@ -4,10 +4,7 @@
  * [929] 独特的电子邮件地址
  */
 
-// 暴力解法
-// 利用split切割邮箱，得到数组的第二个值为域名
-// 利用split和正则表达式去除相关的字符
-// 利用obj对象的特性进行存储，最后遍历obj对象即可
+
 
 // @lc code=start
 /**
@@ -15,7 +12,11 @@
  * @return {number}
  */
 var numUniqueEmails = function(emails) {
-  const obj = {}
+  // 暴力解法
+  // 利用split切割邮箱，得到数组的第二个值为域名
+  // 利用split和正则表达式去除相关的字符
+  // 利用obj对象的特性进行存储，最后遍历obj对象即可
+  /* const obj = {}
   emails.forEach(item => {
     const data = item.split('@');
     const param = data[0].split("+")[0].replace(/\./g, '');
@@ -33,7 +34,17 @@ var numUniqueEmails = function(emails) {
       sum ++
     }
   }
-  return sum
+  return sum */
+
+  // 利用map来存储结果，减少时间复杂度
+  const map = new Map()
+  emails.forEach(item => {
+    const data = item.split('@');
+    const param = data[0].split("+")[0].replace(/\./g, '');
+    const host = data[1]
+    map.set(param+'@'+host, true)
+  })
+  return map.size;
 };
 // @lc code=end
 
